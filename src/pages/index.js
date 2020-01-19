e5dad6b0fe588f4a60ff464a6fd453ad95097bc3
Arlen Peiffer <arlenpeiffer@gmail.com>
2020-01-19 10:58:14 -0800

    fixes path to SEO component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Global } from "@emotion/core"
 import Resume from "../components/Resume"
-import SEO from "../components/SEO"
+import SEO from "../components/seo"
 import globalStyles from "../constants/globalStyles"
 
 const IndexPage = () => (
